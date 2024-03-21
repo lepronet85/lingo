ceb75daf27f7baf701cd6ffa9ef9802d26c17f1b
lepronet85 <lepronet85@gmail.com>
2024-03-21 16:01:01 +0100

    Done with variants buttons

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,26 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-white text-black border border-slate-200 border-2 border-b-4",
+        default:
+          "bg-white text-black border border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100",
+        primary:
+          "bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-2",
+        primaryOutline: "bg-white text-sky-500 hover:bg-slate-100",
+        secondary:
+          "bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-2",
+        secondaryOutline: "bg-white text-green-600 hover:bg-slate-100",
+        danger:
+          "bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-rose-600 border-b-4 active:border-b-2",
+        dangerOutline: "bg-white text-rose-600 hover:bg-slate-100",
+        super:
+          "bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-2",
+        superOutline: "bg-white text-indigo-600 hover:bg-slate-100",
+        ghost:
+          "bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100",
+        sidebar:
+          "bg-transparent text-slate-500 border-transparent border-2 hover:bg-slate-100 transition-none",
+        sidebarOutline:
+          "bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none",
       },
       size: {
         default: "h-11 px-4 py-2",
@@ -21,7 +40,7 @@ const buttonVariants = cva(
     },
     defaultVariants: {
       variant: "default",
-      size: "default", 
+      size: "default",
     },
   }
 );
