756fa4fd0307478ddbfd5ecea42d0b1e449c7f96
lepronet85 <lepronet85@gmail.com>
2024-03-21 14:40:07 +0100

    Create button variants

---
+++ app/buttons/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const ButtonsPage = () => {
+  return (
+    <div className="p-4 space-y-4 flex flex-col">
+      <Button>Primary</Button>
+      <Button>Primary outline</Button>
+    </div>
+  );
+};
+
+export default ButtonsPage;
