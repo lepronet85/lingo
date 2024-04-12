5df4174543f29f2def5951193e192b1845233af3
lepronet85 <lepronet85@gmail.com>
2024-04-12 11:54:37 +0100

    Update

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" fill alt="Hero" />
+      </div>
+      <div className="lex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, practice, and master new languages with Lingo
+        </h1>
+      </div>
+    </div>
+  );
+}
